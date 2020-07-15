6bb8c94b884c86d6a6df1971816f16121425575d
Kyle <kavants@gmail.com>
2020-07-15 12:59:23 -0500

    fixed bandit1.sh

--- bandit/bandit1.sh
+++ bandit/bandit1.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/expect -f
 
-spawn ssh -l bandit0 -p 2220 bandit.labs.overthewire.org
+spawn ssh -l bandit1 -p 2220 bandit.labs.overthewire.org
 expect {
     "*yes/no*" { send "yes\r" ; exp_continue }
-    "*assword:" { send "bandit0\r" ; }
+    "*assword:" { send "boJ9jbbUNNfktd78OOpsqOltutMc3MY1\r" ; }
 }
 expect "*\$ "
 
