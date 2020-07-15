2ab7952fbcf3e3a90049c28d00681ea07f32da9d
Kyle <kavants@gmail.com>
2020-07-15 11:38:23 -0500

    removed second exp_continue so script continues immediately after logging in

--- bandit/bandit1.sh
+++ bandit/bandit1.sh
@@ -3,7 +3,7 @@
 spawn ssh -l bandit0 -p 2220 bandit.labs.overthewire.org
 expect {
     "*yes/no*" { send "yes\r" ; exp_continue }
-    "*assword:" { send "bandit0\r" ; exp_continue }
+    "*assword:" { send "bandit0\r" ; }
 }
 expect "*\$ "
 
