f6e99e98cc17fcd23939b10409067b779e7c816c
Kyle <kavants@gmail.com>
2020-08-13 23:33:37 -0500

    added notes to bandit0 and bandit1

--- bandit/bandit00.sh
+++ bandit/bandit00.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/expect -f
 
+#used to ssh and login
 spawn ssh -l bandit0 -p 2220 bandit.labs.overthewire.org
 expect {
     "*yes/no*" { send "yes\r" ; exp_continue }
     "*assword:" { send "bandit0\r" ; }
 }
-expect "*\$ "
 
+#display contents of the file
+expect "*\$ "
 send "cat readme\r"
 expect "*\$ "
 
--- bandit/bandit01.sh
+++ bandit/bandit01.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/expect -f
 
+
+#used to ssh and login
 spawn ssh -l bandit1 -p 2220 bandit.labs.overthewire.org
 expect {
     "*yes/no*" { send "yes\r" ; exp_continue }
@@ -7,6 +9,7 @@ expect {
 }
 expect "*\$ "
 
+#display contents of a file named -
 send "cat ./-\r"
 expect "*\$ "
 
